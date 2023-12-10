df49884ac16c51b34ffaddab0d48276f00aaf7f2
Aditya Bayu <97433258+iniadittt@users.noreply.github.com>
2023-12-10 23:24:44 +0700

    update: all fitur and content

--- components/Fitur.tsx
+++ components/Fitur.tsx
@@ -1,20 +1,29 @@
-import FiturCard from '@/components/FiturCard'
+import FiturCard from "@/components/FiturCard";
 
 export default function Fitur() {
   return (
-    <div id='Fitur' className="lg:w-full lg:h-auto lg:pt-32 lg:px-14 py-12 px-4">
-        <div className='lg:w-[1200px] lg:mx-auto'>
-            <h1 className="lg:w-full font-bold text-4xl text-center lg:mb-20 mb-10">Fitur Aplikasi</h1>
-            <div className="lg:flex lg:flex-row lg:flex-wrap lg:justify-between lg:gap-20 flex flex-col gap-10">
-                <FiturCard title="Machine" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates distinctio minus error maxime eaque! Molestiae iste enim perferendis? Nemo, sint."/>
-                <FiturCard title="Machine" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates distinctio minus error maxime eaque! Molestiae iste enim perferendis? Nemo, sint."/>
-                <FiturCard title="Machine" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates distinctio minus error maxime eaque! Molestiae iste enim perferendis? Nemo, sint."/>
-                <FiturCard title="Machine" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates distinctio minus error maxime eaque! Molestiae iste enim perferendis? Nemo, sint."/>
-                <FiturCard title="Machine" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates distinctio minus error maxime eaque! Molestiae iste enim perferendis? Nemo, sint."/>
-                <FiturCard title="Machine" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates distinctio minus error maxime eaque! Molestiae iste enim perferendis? Nemo, sint."/>
-            </div>
+    <div
+      id="Fitur"
+      className="lg:w-full lg:h-auto lg:pt-32 lg:px-14 py-12 px-4"
+    >
+      <div className="lg:w-[1200px] lg:mx-auto">
+        <h1 className="lg:w-full font-bold text-4xl text-center lg:mb-20 mb-10">
+          Fitur Aplikasi
+        </h1>
+        <div className="lg:flex lg:flex-row lg:flex-wrap lg:justify-between lg:gap-20 flex flex-col gap-10">
+          <FiturCard
+            title="Deteksi Kesegaran Ikan (A, B, dan C)"
+            description="{Foto}"
+          />
+          <FiturCard title="Prediksi Harga Ikan" description="{Foto}" />
+          <FiturCard
+            title="Keputusan Berlayar untuk Nelayan"
+            description="{Foto}"
+          />
+          <FiturCard title="Pembelian Ikan" description="{Foto}" />
+          <FiturCard title="Penjualan Ikan" description="{Foto}" />
         </div>
+      </div>
     </div>
-    
-  )
+  );
 }
--- components/Team.tsx
+++ components/Team.tsx
@@ -1,20 +1,61 @@
-import TeamCard from '@/components/TeamCard'
-import FotoUser from "@/public/user.jpg"
+import TeamCard from "@/components/TeamCard";
+import FotoUser from "@/public/user.jpg";
 
 export default function Team() {
   return (
-    <div id='Team' className="lg:w-full lg:h-auto lg:mt-20 lg:pt-20 lg:pb-32 lg:px-14 px-4 my-12 py-8">
-        <div className="lg:w-[1200px] lg:mx-auto">
-            <h1 className="w-full font-bold text-4xl text-center lg:mb-16 mb-10">Bloomy Team</h1>
-            <div className="flex lg:flex-row flex-col lg:flex-wrap lg:justify-between lg:gap-12 gap-4">
-                <TeamCard nama="Aditya Bayu | Universitas Muhammadiyah Cirebon" title={["Teknik Informatika", "Cloud Computing"]} github="https://www.github.com/iniadittt" image={FotoUser} />
-                <TeamCard nama="Aditya Bayu | Universitas Muhammadiyah Cirebon" title={["Teknik Informatika", "Cloud Computing"]} github="https://www.github.com/iniadittt" image={FotoUser} />
-                <TeamCard nama="Aditya Bayu | Universitas Muhammadiyah Cirebon" title={["Teknik Informatika", "Cloud Computing"]} github="https://www.github.com/iniadittt" image={FotoUser} />
-                <TeamCard nama="Aditya Bayu | Universitas Muhammadiyah Cirebon" title={["Teknik Informatika", "Cloud Computing"]} github="https://www.github.com/iniadittt" image={FotoUser} />
-                <TeamCard nama="Aditya Bayu | Universitas Muhammadiyah Cirebon" title={["Teknik Informatika", "Cloud Computing"]} github="https://www.github.com/iniadittt" image={FotoUser} />
-                <TeamCard nama="Aditya Bayu | Universitas Muhammadiyah Cirebon" title={["Teknik Informatika", "Cloud Computing"]} github="https://www.github.com/iniadittt" image={FotoUser} />
-            </div>
+    <div
+      id="Team"
+      className="lg:w-full lg:h-auto lg:mt-20 lg:pt-20 lg:pb-32 lg:px-14 px-4 my-12 py-8"
+    >
+      <div className="lg:w-[1200px] lg:mx-auto">
+        <h1 className="w-full font-bold text-4xl text-center lg:mb-16 mb-10">
+          Bloomy Team
+        </h1>
+        <div className="flex lg:flex-row flex-col lg:flex-wrap lg:justify-between lg:gap-12 gap-4">
+          <TeamCard
+            nama="Heical Chandra Syahputra | Politeknik Negeri Jakarta"
+            title={["Teknik Informatika", "Machine Learning"]}
+            github="https://github.com/Dynavx"
+            image={FotoUser}
+          />
+          <TeamCard
+            nama="Andra Rizki Pratama | Politeknik Negeri Jakarta"
+            title={["Teknik Informatika", "Machine Learning"]}
+            github="https://www.github.com/nulitas"
+            image={FotoUser}
+          />
+          <TeamCard
+            nama="Novebri Tito Ramadhani | Universitas Negeri Yogyakarta"
+            title={["Teknik Informatika", "Machine Learning"]}
+            github="https://github.com/Benedixx"
+            image={FotoUser}
+          />
+          <TeamCard
+            nama="Aditya Bayu Aji | Universitas Muhammadiyah Cirebon"
+            title={["Teknik Informatika", "Cloud Computing"]}
+            github="https://www.github.com/iniadittt"
+            image={FotoUser}
+          />
+          <TeamCard
+            nama="Asrini Salsabila Putri | Universitas Siliwangi"
+            title={["Pendidikan Matematika", "Cloud Computing"]}
+            github="https://github.com/asrinisp"
+            image={FotoUser}
+          />
+          <TeamCard
+            nama="Ahmad Tiova Ian Avola | Universitas Muhammadiyah Malang"
+            title={["Teknik Informatika", "Mobile Development"]}
+            github="https://github.com/VBeatDead"
+            image={FotoUser}
+          />
+          <TeamCard
+            nama="Sandhi Karunia Sugihartana | Politeknik Negeri Jakarta"
+            title={["Teknik Informatika", "Mobile Development"]}
+            github="https://github.com/Lowl16"
+            image={FotoUser}
+          />
         </div>
+      </div>
     </div>
-  )
+  );
 }
